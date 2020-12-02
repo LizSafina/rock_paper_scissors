860490fdf4cc92a5108b04bb7e09dbc6fea2cfdc
Safina Lisa <liz.safina@gmail.com>
2020-12-02 05:27:57 -0500

    some updates

--- rps.js
+++ rps.js
@@ -1,22 +1,26 @@
 
                     
                     
-                    
-                    let rock = "rock";
-                    let paper = "paper";
-                    let scissors = "scissors";
+            
+                    let rock = document.getElementById('rockButton');
+                    let paper = document.getElementById('paperButton');
+                    let scissors = document.getElementById('scissorsButton');
                    
+                    rock.addEventListener('click', playRound);
+                    paper.addEventListener('click', playRound);
+                    scissors.addEventListener('click',playRound);
+                    
 
 
                 function computerPlay(){
                     
                     let compChoice = Math.floor(Math.random() * 3)
                     if (compChoice == 0){
-                        return rock;
+                        return "rock";
                     }if (compChoice == 1){
-                        return paper;
+                        return "paper";
                     }if (compChoice == 2){
-                        return scissors;
+                        return "scissors";
 
                     }
                         
@@ -26,33 +30,43 @@
                 function playRound(playerSelection, computerSelection){
                     let gameOn = true;
                     while(gameOn){
+                        if(rock.onclick){
+                            playerSelection = "rock";
+                        }if(paper.onclick){
+                            playerSelection = "paper"
+                        }else{
+                            playerSelection = "scissors"
+                        }
+                    
                             computerSelection = computerPlay();
-                            let myChoice = prompt("Your choice?");
-                            playerSelection = myChoice.toLowerCase();
-                if(playerSelection == rock && computerSelection ==paper){
+                            
+                if(playerSelection == "rock" && computerSelection =="paper"){
                     return result = "You lost Paper beats rock!";
-                }if(playerSelection == paper && computerSelection == rock){
+                }if(playerSelection == "paper" && computerSelection == "rock"){
                     return result = "You win Paper beats rock";
-                }if(playerSelection == scissors && computerSelection == rock){
+                }if(playerSelection == "scissors" && computerSelection == "rock"){
                     return result = "You lost Rock beats scissors!"
-                }if( playerSelection ==rock && computerSelection == scissors){
+                }if( playerSelection =="rock" && computerSelection == "scissors"){
                     return result = "You win Rock beats scissors!"
-            }if(playerSelection == scissors && computerSelection == paper){
+            }if(playerSelection == "scissors" && computerSelection == "paper"){
               return result =  "You win Scissors beats paper!"
-            }if(playerSelection ==paper && computerSelection ==scissors){
+            }if(playerSelection =="paper" && computerSelection =="scissors"){
                 return result = "You lost Scissors beats paper!"
             }
         if(playerSelection == computerSelection){
            gameOn = false;
-           gameOn = true;
+           return result = "It's a tie";
+          
         }
+        
     }
+    
 }
-
+/*
         function game(){
             let playerScore = 0;
              let compScore= 0;  
-            for(let i = 1; i <= 5; i++){
+            for(let i = 1; i <=5 ; i++){
                 playRound();
                 if (result.includes("win")){
                     playerScore++;
@@ -61,7 +75,7 @@
                     compScore++;
                 }  
                 console.log(playerScore + " : " + compScore);
-            }
+            }                                                                   
             
             if (playerScore > compScore){
                 console.log ("You're the winner");
@@ -71,6 +85,7 @@
 
         }
 
+    */
         
 
 
